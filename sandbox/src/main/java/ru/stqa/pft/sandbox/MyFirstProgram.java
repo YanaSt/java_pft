0a52f04745951dc99746bb46cd9015c4084e8387
YanaSt <31668836+YanaSt@users.noreply.github.com>
2017-09-09 20:01:58 +0300

    fix

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,11 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		String som = "world";
+		System.out.println("Hello, " + som + "!");
+
+		int l = 4;
+		int s = l * l;
+		System.out.println("Площадь квадрата со стороной " + l + "=" + s);
 	}
 }
